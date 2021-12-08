18fafc65bf6f9f1d27dc5aea9a60b83a6ac06297
adnan <mechcoder@pop-os.localdomain>
2021-12-08 16:45:49 +0530

    add content for each separate note

--- app/routes/notes/$noteId.tsx
+++ app/routes/notes/$noteId.tsx
@@ -1,3 +1,31 @@
+import { Note } from ".prisma/client";
+import { LoaderFunction, useCatch, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const note: Note | null = await db.note.findUnique({
+    where: { id: params.noteId },
+  });
+  if (!note) {
+    throw new Response("Note not found", { status: 404 });
+  }
+  return note;
+};
+
 export default function Note() {
-  return <p>This is the note route</p>;
+  const note = useLoaderData<Note>();
+  return (
+    <section>
+      <h1>{note.title}</h1>
+      <p>{note.content}</p>
+    </section>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  switch (caught.status) {
+    case 404:
+      return <p>Nice try, but there's no such note found 😏</p>;
+  }
 }
